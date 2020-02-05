8644590590a54c9893b85219444e7b24e2bd3182
Christopher Batey <christopher.batey@gmail.com>
2020-02-05 13:02:42 +0000

    Code action / quickfix for import not working for nested class

---
+++ src/main/scala/info/batey/ImportIssue.scala
@@ -0,0 +1,9 @@
+package info.batey
+
+
+
+object Nested {
+  // move this out of Nested and the code action
+  // import will work
+  case class Sausage(s: String)
+}
---
+++ src/main/scala/info/batey/call/CallSite.scala
@@ -0,0 +1,6 @@
+package info.batey.call
+
+object CallSite {
+    // No quickfix available unless Sausage case class is moved out of the Nested object
+    Sausage("Hello") 
+}
